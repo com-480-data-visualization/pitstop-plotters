016e000f1a9bcc08ef805cf27a9aacc5e4453d07
Xavier <xavier.ogay@epfl.ch>
2024-04-25 14:41:11 +0200

    Add many modules

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,20 @@
 import style from "./Styles.module.css";
 import WelcomePage from "./Welcome/WelcomePage";
-import TemplatePage from "./Template/TemplatePage";
+import LeaderboardPage from "./Leaderboard/LeaderboardPage";
+import HallOfFamePage from "./HallOfFame/HallOfFamePage";
+import CircuitsPage from "./Circuits/CircuitsPage";
+import SeasonsEvolutionPage from "./SeasonsEvolution/SeasonsEvolutionPage";
+import DriverTeamRelationsPage from "./DriverTeamRelations/DriverTeamRelationsPage";
 
 function App() {
   return (
       <div className={style.container}>
         <div className={style.section}><WelcomePage /></div>
-        <div className={style.section}><TemplatePage /></div>
+        <div className={style.section}><LeaderboardPage /></div>
+        <div className={style.section}><HallOfFamePage /></div>
+        <div className={style.section}><CircuitsPage /></div>
+        <div className={style.section}><SeasonsEvolutionPage /></div>
+        <div className={style.section}><DriverTeamRelationsPage /></div>
       </div>
   );
 }
---
+++ website/src/Leaderboard/LeaderboardPage.jsx
@@ -0,0 +1,18 @@
+import styles from "./Leaderboard.module.css"
+import leftImage from '../img/apex_left.png';
+import rightImage from '../img/apex_right.png';
+
+const LeaderboardPage = () => {
+    return (
+        <div className={styles.template}>
+            <div className={styles.border}>
+                <img src={leftImage} alt="Left Border" className={styles.boder_img}/>
+                <div className={styles.content}>
+                </div>
+                <img src={rightImage} alt="Right Border" className={styles.boder_img}/>
+            </div>
+        </div>
+    );
+}
+
+export default LeaderboardPage;
