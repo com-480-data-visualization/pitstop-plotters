ef90dd3e75ef4453e065522af3c0cd3bcc4470fd
mac <yahya.hadi@epfl.ch>
2024-04-25 21:03:56 +0200

    add rest of vis

--- website/src/Circuits/CircuitsPage.jsx
+++ website/src/Circuits/CircuitsPage.jsx
@@ -1,6 +1,7 @@
 import styles from "./Circuits.module.css"
 import leftImage from '../img/apex_left.png';
 import rightImage from '../img/apex_right.png';
+import Circuits from "./Circuits";
 
 const CircuitsPage = () => {
     return (
@@ -9,6 +10,7 @@ const CircuitsPage = () => {
                 <img src={leftImage} alt="Left Border" className={styles.boder_img}/>
                 <div className={styles.content}>
                     <h1>Circuits</h1>
+                    <Circuits />
                 </div>
                 <img src={rightImage} alt="Right Border" className={styles.boder_img}/>
             </div>
--- website/src/Leaderboard/LeaderboardPage.jsx
+++ website/src/Leaderboard/LeaderboardPage.jsx
@@ -1,6 +1,7 @@
 import styles from "./Leaderboard.module.css"
 import leftImage from '../img/apex_left.png';
 import rightImage from '../img/apex_right.png';
+import Leaderboard from "./Leaderboard";
 
 const LeaderboardPage = () => {
     return (
@@ -9,6 +10,7 @@ const LeaderboardPage = () => {
                 <img src={leftImage} alt="Left Border" className={styles.boder_img}/>
                 <div className={styles.content}>
                     <h1>Leaderboard</h1>
+                    <Leaderboard />
                 </div>
                 <img src={rightImage} alt="Right Border" className={styles.boder_img}/>
             </div>
