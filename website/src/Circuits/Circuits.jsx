1aeb770a1879eab6c9019b6fca182ae686310f3f
ColinPelletier <colinpelletier@outlook.com>
2024-05-28 23:00:32 +0200

    add new map with zoom and default markers (markers are not interactive yet!)

--- website/src/Circuits/Circuits.jsx
+++ website/src/Circuits/Circuits.jsx
@@ -1,38 +1,77 @@
-import React, { useEffect } from "react";
-import * as d3 from "d3";
-import * as topojson from "topojson"; // Import topojson library
-import world_110m from './world-110m.json';
+import React from "react"
+import { ComposableMap, Geographies, Geography, Marker, ZoomableGroup } from "react-simple-maps"
+// import geoUrl from './features.json';
+const geoUrl = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json"
+
+
+const markers = [
+    {
+      markerOffset: -30,
+      name: "Buenos Aires",
+      coordinates: [-58.3816, -34.6037]
+    },
+    { markerOffset: 15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
+    { markerOffset: 15, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
+    { markerOffset: 15, name: "Santiago", coordinates: [-70.6693, -33.4489] },
+    { markerOffset: 15, name: "Bogota", coordinates: [-74.0721, 4.711] },
+    { markerOffset: 15, name: "Quito", coordinates: [-78.4678, -0.1807] },
+    { markerOffset: -30, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
+    { markerOffset: -30, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
+    { markerOffset: 15, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
+    { markerOffset: 15, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
+    { markerOffset: 15, name: "Caracas", coordinates: [-66.9036, 10.4806] },
+    { markerOffset: 15, name: "Lima", coordinates: [-77.0428, -12.0464] }
+  ];
+
 
 const Circuits = () => {
-    useEffect(() => {
+  return (
+    <ComposableMap
+    //   projection="geoAzimuthalEqualArea"
+    //   projectionConfig={{
+    //     rotate: [58, 20, 0],
+    //     scale: 400
+    //   }}
+    >
+        <ZoomableGroup center={[0, 0]} zoom={1}>
+        <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+            geographies.map((geo) => (
+                <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#EAEAEC"
+                stroke="#D6D6DA"
+                />
+            ))}
+        </Geographies>
+        {markers.map(({ name, coordinates, markerOffset }) => (
+            <Marker key={name} coordinates={coordinates}>
+            <g
+                fill="none"
+                stroke="#FF5533"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                transform="translate(-12, -24)"
+                key={name}
+            >
+                <circle cx="12" cy="10" r="3" />
+                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+            </g>
+            <text
+                textAnchor="middle"
+                y={markerOffset}
+                style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+            >
+                {name}
+            </text>
+            </Marker>
+        ))}
 
-       // Remove the svg if it already exists
-        const svg = d3.select("#globe"); // Select the div with id "globe"
-        const width = +svg.attr("width");
-        const height = +svg.attr("height");
-
-        // Map and projection
-        const projection = d3.geoNaturalEarth1()
-            .scale(width / 1.3 / Math.PI)
-            .translate([width / 2, height / 2]);
-
-        // Draw the map
-        svg.append("g")
-            .selectAll("path")
-            .data(topojson.feature(world_110m, world_110m.objects.countries).features) // Use topojson to convert the GeoJSON data
-            .enter().append("path")
-            .attr("fill", "#69b3a2")
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .style("stroke", "#fff");
-    }, []); // Run only once on component mount
-
-    return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-            <svg id="globe" width="800" height="500"></svg> 
-        </div>
-    );
-};
+        </ZoomableGroup>
+    </ComposableMap>
+  )
+}
 
 export default Circuits;
--- website/src/SeasonsEvolution/SeasonsEvolutionPage.jsx
+++ website/src/SeasonsEvolution/SeasonsEvolutionPage.jsx
@@ -8,12 +8,23 @@ const SeasonsEvolutionPage = () => {
     return (
         <div className={styles.seasonevolution}>
             <div className={styles.border}>
-                <img src={leftImage} alt="Left Border" className={styles.boder_img}/>
+                <img src={leftImage} alt="Left Border" className={styles.boder_img} />
                 <div className={styles.content}>
-                    <h1>Seasons Evolution</h1>
-                    <SeasonEvolution />
+                    <div className={styles.titleWrapper}>
+                        <h2 className={styles.title}>Seasons Evolution</h2>
+                    </div>
+                    <div className={styles.left}>
+                        <SeasonEvolution />
+                    </div>
+                    <div className={styles.right}>
+                        <p>
+                            This page shows the evolution of the number of seasons per year for the top 10 TV shows.
+                            The data is taken from the TVmaze API and the visualization is done using d3.js.
+                            The data is updated every 24 hours.
+                        </p>
+                    </div>
                 </div>
-                <img src={rightImage} alt="Right Border" className={styles.boder_img}/>
+                <img src={rightImage} alt="Right Border" className={styles.boder_img} />
             </div>
         </div>
     );
