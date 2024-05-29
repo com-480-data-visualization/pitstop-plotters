0083ef43f939335051a2b1d631d87b64ed12c545
Xavier Ogay <xaga.ogay@gmail.com>
2024-05-29 19:07:14 +0200

    Improved character selection

--- website/src/HallOfFame/HallOfFameSelection.jsx
+++ website/src/HallOfFame/HallOfFameSelection.jsx
@@ -1,33 +1,70 @@
 import React, { useState } from 'react';
 import styles from './HallOfFameSelection.module.css';
-import image1 from '../img/Pilotes/Clark.png';
-import image2 from '../img/Pilotes/Fagio.png';
-import image3 from '../img/Pilotes/SENNA_s.png';
-import image4 from '../img/Pilotes/SCH.png';
-import image5 from '../img/Pilotes/HAM.png';
+
+// Import images for normal and selected states
+import clark from '../img/Pilotes/Clark.png';
+import clarkSelected from '../img/Pilotes/Clark_s.png';
+import fagio from '../img/Pilotes/Fagio.png';
+import fagioSelected from '../img/Pilotes/Fagio_s.png';
+import sch from '../img/Pilotes/SCH_p.png';
+import schSelected from '../img/Pilotes/SCH_ps.png';
+import senna from '../img/Pilotes/SENNA_p.png';
+import sennaSelected from '../img/Pilotes/SENNA_ps.png';
+import ham from '../img/Pilotes/HAM_p.png';
+import hamSelected from '../img/Pilotes/HAM_ps.png';
 
 function ImageRow() {
     const [selectedImage, setSelectedImage] = useState(null);
 
-    const images = [image1, image2, image3, image4, image5];
+    const images = [
+        { name: 'Clark', normal: clark, selected: clarkSelected },
+        { name: 'Fagio', normal: fagio, selected: fagioSelected },
+        { name: 'Schumacher', normal: sch, selected: schSelected },
+        { name: 'Senna', normal: senna, selected: sennaSelected },
+        { name: 'Hamilton', normal: ham, selected: hamSelected }
+    ];
 
     const toggleSize = index => {
         setSelectedImage(selectedImage === index ? null : index);
     };
 
+    const getImageSrc = (image, index) => {
+        return selectedImage === index ? image.selected : image.normal;
+    };
+
+    const getTextStyle = (index) => {
+        if (selectedImage === index) {
+            return {
+                fontWeight: 'bold',
+                fontSize: '24px',
+                WebkitTextStroke: '1px #EF1A2D',
+                color: '#EF1A2D',
+                textAlign: 'center'
+            };
+        } else {
+            return {
+                fontSize: '16px',
+                color: 'white',
+                textAlign: 'center'
+            };
+        }
+    };
+
     return (
         <div className={styles.imageRow}>
             {images.map((image, index) => (
-                <img
-                    key={index}
-                    src={image}
-                    alt={`Image ${index}`}
-                    style={{
-                        width: selectedImage === index ? '200px' : '100px',
-                        height: 'auto'
-                    }}
-                    onClick={() => toggleSize(index)}
-                />
+                <div key={index} style={{ textAlign: 'center' }}>
+                    <div style={getTextStyle(index)}>{image.name}</div>  {/* Conditional styling */}
+                    <img
+                        src={getImageSrc(image, index)}
+                        alt={`Image ${image.name}`}
+                        style={{
+                            width: selectedImage === index ? '200px' : '100px',
+                            height: 'auto'
+                        }}
+                        onClick={() => toggleSize(index)}
+                    />
+                </div>
             ))}
         </div>
     );
