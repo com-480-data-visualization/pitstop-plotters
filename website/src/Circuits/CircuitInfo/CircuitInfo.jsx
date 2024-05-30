c94d42bf7d4d85fcbbc288cb83d4ac194fa1d0cf
ColinPelletier <colinpelletier@outlook.com>
2024-05-30 16:15:59 +0200

    improve country info module

--- website/src/Circuits/CircuitInfo/CircuitInfo.jsx
+++ website/src/Circuits/CircuitInfo/CircuitInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./CircuitInfo.module.css"
-import line from "../../img/line1.png"
+import lineImage from '../../img/line1.png';
+import circuitImage from "../../img/Circuits/Melbourne.jpg"
 
 const CircuitInfo = ({ circuitId }) => {
     // TODO add the code to parse the file and get the circuit data
@@ -13,27 +14,37 @@ const CircuitInfo = ({ circuitId }) => {
         "gpHeld": "27",
         "lastWinner": "Carlos Sainz",
         "fastestLap": "1'19\"813 (Charles Leclerc)",
-        "circuitImage": "../img/Circuits/Melbourne.jpg"
+        "circuitImage": circuitImage
     };
 
     return (
         <div className={styles.infobox}>
+            {/* content  */}
             <div className={styles.infoContainer}>
                 <h2 className={styles.title}>{circuitData.name}</h2>
                 <div className={styles.infoColumn}>
-                    <p className={styles.info}><strong>Country:</strong> {circuitData.country}</p>
-                    <p className={styles.info}><strong>Location:</strong> {circuitData.location}</p>
+                    <p className={styles.info}>
+                        <strong className={styles.infoDescription}>Country:&ensp;</strong>{circuitData.country} <br/>
+                        <strong className={styles.infoDescription}>Location:&ensp;</strong>{circuitData.location}
+                    </p>
                 </div>
                 <div className={styles.infoColumn}>
-                    <p className={styles.info}><strong>Circuit Length:</strong> {circuitData.length} km</p>
-                    <p className={styles.info}><strong>Grand-Prix held:</strong> {circuitData.gpHeld}</p>
+                    <p className={styles.info}>
+                        <strong className={styles.infoDescription}>Circuit Length:&ensp;</strong>{circuitData.length} km<br/>
+                        <strong className={styles.infoDescription}>Grand-Prix held:&ensp;</strong>{circuitData.gpHeld}
+                    </p>
                 </div>
                 <div className={styles.infoColumn}>
-                    <p className={styles.info}><strong>Last Winner:</strong> {circuitData.lastWinner}</p>
-                    <p className={styles.info}><strong>Fastest Lap:</strong> {circuitData.fastestLap}</p>
+                    <p className={styles.info}>
+                        <strong className={styles.infoDescription}>Last Winner:&ensp;</strong>{circuitData.lastWinner}<br/>
+                        <strong className={styles.infoDescription}>Fastest Lap:&ensp;</strong>{circuitData.fastestLap}
+                    </p>
                 </div>
+                <img src={lineImage} alt="Center line" className={styles.centerImage}/>
             </div>
+            
 
+            {/* circuit layout  */}
             <div className={styles.imageContainer}>
                 <img src={circuitData.circuitImage} alt={`${circuitData.name} Circuit`} className={styles.circuitImage} />
             </div>
